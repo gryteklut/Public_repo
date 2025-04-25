c47e14d08a1faa87f72fbd08a1cf2466a433b6fe
gryteklut <130316387+gryteklut@users.noreply.github.com>
2025-04-25 14:08:29 +0200

    Update ChangePasswordWithResetGroupID.ps1

--- EntraID/ChangePasswordWithResetGroupID.ps1
+++ EntraID/ChangePasswordWithResetGroupID.ps1
@@ -5,30 +5,24 @@
 $groupid = "9a01c262-c222-4bc9-b729-e29dd3722a72" #ObjectID of the group
 
 $users = Get-MgGroupMember -GroupId $groupId
-
 foreach ($user in $users) {
     $user = get-mguser -UserId $user.id
     $userupn = $user.userPrincipalName
     write-host $userupn
 
-    $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#-?".ToCharArray()
-    $num  = "123456789".ToCharArray()
-    $getnum = $num | Get-Random -Count 2
-    $randomString = -join $getnum
-     for ($i = 0; $i -lt 24; $i++) {
-     $randomChar = $chars | Get-Random
-     $randomString += $randomChar
-     }
-    #Write-Host $randomString #Uncomment if you want to see the password
+    #Create a new password
+    Add-Type -AssemblyName 'System.Web'
+    $NewPassword = [System.Web.Security.Membership]::GeneratePassword(24, 3)
+
+    #Write-Host $NewPassword #Uncomment to view new password
 
 
     # Create the password profile with the generated password
     $passwordprofile = @{
-        Password = $randomString
+        Password = $NewPassword
         forceChangePasswordNextSignIn = $False
         forceChangePasswordNextSignInWithMfa = $False
     }
-
     # Set the user's password using MgUser
     Update-MgUser -UserId $userupn -PasswordProfile $passwordprofile
 }
